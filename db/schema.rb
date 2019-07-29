880c79c13cf6ec77a75ac82b85410c7d66dc1e6d
benevina-santiago <benevina.santiago@gmail.com>
2019-07-29 15:01:11 -0700

    Integrate index action for portfolio items

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2019_07_27_035010) do
 
   #These are extensions that must be enabled in order to support this database
+  
   enable_extension "plpgsql"
 
   create_table "blogs", force: :cascade do |t|
