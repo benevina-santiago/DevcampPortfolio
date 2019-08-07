f591e67c5d1c05d330b359e6c6c3068d79991e2f
benevina-santiago <benevina.santiago@gmail.com>
2019-08-07 12:57:51 -0700

    Update body in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
-    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus mauris purus, congue a consectetur a, porta quis massa. Quisque vitae lobortis lectus. Aenean vehicula ut turpis nec suscipit. Praesent sit amet venenatis massa, at iaculis ex. Aenean ac volutpat nunc. Morbi lacinia purus vel ante dapibus lacinia. Etiam diam erat, auctor ac suscipit vel, pretium a nisl. Praesent egestas auctor elementum. Nulla facilisi. Quisque vulputate semper velit eu ultrices. Suspendisse hendrerit, libero eu mattis aliquet, quam justo faucibus neque, vitae maximus nunc ante egestas ante. Donec malesuada magna sem, in efficitur mauris ullamcorper ac. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque vel est mi."
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus mauris purus, congue a consectetur a, porta quis massa. Quisque vitae lobortis lectus."
     )
 end
 
@@ -20,7 +20,7 @@ puts "5 skills created"
   Portfolio.create!(
     title: "Portfolio title: #{portfolio_item}" ,
     subtitle: "My great service",
-    body: "Nam efficitur pulvinar enim, eu semper elit rhoncus eu. Etiam sed nunc tellus. Aenean laoreet, elit ac feugiat vestibulum, massa justo rutrum risus, a pulvinar augue ex auctor purus. In ut velit in nunc lacinia posuere nec a risus. In bibendum feugiat enim eget volutpat.",
+    body: "Nam efficitur pulvinar enim, eu semper elit rhoncus eu. Etiam sed nunc tellus. Aenean laoreet, elit ac feugiat vestibulum, massa justo rutrum risus, a pulvinar augue ex auctor purus.",
     main_image: "http://via.placeholder.com/600x200",
     thumb_image: "http://via.placeholder.com/350x200"
     )
