c6544cd47575739fe468fb061bf68dcbdbc05849
benevina-santiago <benevina.santiago@gmail.com>
2019-08-08 14:53:40 -0700

    Implete concern for managing data

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_07_213233) do
-
-
-
+ActiveRecord::Schema.define(version: 2019_08_08_213751) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -53,6 +50,7 @@ ActiveRecord::Schema.define(version: 2019_08_07_213233) do
     t.integer "percent_utilized"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "badge"
   end
 
   create_table "topics", force: :cascade do |t|
