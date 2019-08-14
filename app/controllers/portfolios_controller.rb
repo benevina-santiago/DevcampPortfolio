08d19c7caee922d044b0f505a1e6b65d241e4834
benevina-santiago <benevina.santiago@gmail.com>
2019-08-13 19:41:35 -0700

    Refactor strong params for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -13,7 +13,7 @@ class PortfoliosController < ApplicationController
   end
 
   def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
+    @portfolio_item = Portfolio.new(portfolio_params)
 
     respond_to do |format|
       if @portfolio_item.save
@@ -32,7 +32,7 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.find(params[:id])
 
     respond_to do |format|
-      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+      if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
       else
         format.html { render :edit }
@@ -55,4 +55,22 @@ def destroy
     end
   end
 
+private # this private indicates that only the following methods can be accessed within this Class PortfolioController. The rest of the methods can be accessed by any other class outside of this file.
+def portfolio_params
+  params.require(:portfolio).permit(:title, 
+                                    :subtitle, 
+                                    :body, 
+                                    technologies_attributes: [:name]
+                                    )
 end
+
+
+end
+
+
+
+
+
+
+
+
